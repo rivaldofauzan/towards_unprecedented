3794f9a7852161cd4cf0584949f4b0e1b81f08ff
unknown <rivaldofr03@gmail.com>
2023-11-17 13:18:30 +0700

    first commit

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,70 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">{{ trans('FDS') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ trans('Fraud Detection') }}</a>
+        </div>
+        <ul class="nav">
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ trans('Dashboard') }}</p>
+                </a>
+            </li>
+            <li>
+                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
+                    <i class="fab fa-laravel" ></i>
+                    <span class="nav-link-text" >{{ trans('Laravel Examples') }}</span>
+                    <b class="caret mt-1"></b>
+                </a>
+
+                <div class="collapse show" id="laravel-examples">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                            <a href="{{ route('profile.edit')  }}">
+                                <i class="tim-icons icon-single-02"></i>
+                                <p>{{ trans('User Profile') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug == 'users') class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="tim-icons icon-bullet-list-67"></i>
+                                <p>{{ trans('User Management') }}</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li @if ($pageSlug == 'icons') class="active " @endif>
+                <a href="{{ route('pages.icons') }}">
+                    <i class="tim-icons icon-atom"></i>
+                    <p>{{ trans('Icons') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'maps') class="active " @endif>
+                <a href="{{ route('pages.maps') }}">
+                    <i class="tim-icons icon-pin"></i>
+                    <p>{{ trans('Maps') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'notifications') class="active " @endif>
+                <a href="{{ route('pages.notifications') }}">
+                    <i class="tim-icons icon-bell-55"></i>
+                    <p>{{ trans('Notifications') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'tables') class="active " @endif>
+                <a href="{{ route('pages.tables') }}">
+                    <i class="tim-icons icon-puzzle-10"></i>
+                    <p>{{ trans('Table List') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'typography') class="active " @endif>
+                <a href="{{ route('pages.typography') }}">
+                    <i class="tim-icons icon-align-center"></i>
+                    <p>{{ trans('Typography') }}</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app', ['page' => trans('User Profile'), 'pageSlug' => 'profile'])
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ trans('Edit Profile') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.update') }}" autocomplete="off">
+                    <div class="card-body">
+                            @csrf
+                            @method('put')
+
+                            @include('alerts.success')
+
+                            <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                <label>{{ trans('Name') }}</label>
+                                <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ trans('Name') }}" value="{{ old('name', auth()->user()->name) }}">
+                                @include('alerts.feedback', ['field' => 'name'])
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <label>{{ trans('Email address') }}</label>
+                                <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ trans('Email address') }}" value="{{ old('email', auth()->user()->email) }}">
+                                @include('alerts.feedback', ['field' => 'email'])
+                            </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ trans('Save') }}</button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ trans('Password') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.password') }}" autocomplete="off">
+                    <div class="card-body">
+                        @csrf
+                        @method('put')
+
+                        @include('alerts.success', ['key' => 'password_status'])
+
+                        <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">
+                            <label>{{ trans('Current Password') }}</label>
+                            <input type="password" name="old_password" class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" placeholder="{{ trans('Current Password') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'old_password'])
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <label>{{ trans('New Password') }}</label>
+                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ trans('New Password') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'password'])
+                        </div>
+                        <div class="form-group">
+                            <label>{{ trans('Confirm New Password') }}</label>
+                            <input type="password" name="password_confirmation" class="form-control" placeholder="{{ trans('Confirm New Password') }}" value="" required>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ trans('Change password') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-user">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="author">
+                            <div class="block block-one"></div>
+                            <div class="block block-two"></div>
+                            <div class="block block-three"></div>
+                            <div class="block block-four"></div>
+                            <a href="#">
+                                <img class="avatar" src="{{ asset('white') }}/img/emilyz.jpg" alt="">
+                                <h5 class="title">{{ auth()->user()->name }}</h5>
+                            </a>
+                            <p class="description">
+                                {{ trans('Ceo/Co-Founder') }}
+                            </p>
+                        </div>
+                    </p>
+                    <div class="card-description">
+                        {{ trans('Do not be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...') }}
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="button-container">
+                        <button class="btn btn-icon btn-round btn-facebook">
+                            <i class="fab fa-facebook"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-twitter">
+                            <i class="fab fa-twitter"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-google">
+                            <i class="fab fa-google-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
